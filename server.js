908844c208a29e751c18fb63541dec32e9c006a3
Jamesbillard12 <jamesbillard12@hotmail.com>
2018-02-06 00:09:14 -0800

    auth has been update

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.GOOGLE_CLIENT_ID,
+      clientSecret: keys.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
